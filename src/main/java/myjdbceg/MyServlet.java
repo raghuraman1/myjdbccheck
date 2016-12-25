6a9d0cf7f3d356c5fdefb0c515f2f4303e1812b6
raghuraman1 <raghuraman1@yahoo.com>
2016-12-26 03:14:47 +0530

    commitg

--- src/main/java/myjdbceg/MyServlet.java
+++ src/main/java/myjdbceg/MyServlet.java
@@ -18,6 +18,48 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Check")
 public class MyServlet extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body><form method=\"POST\">");
+		out.println("<table>");
+		out.println("<tr>");
+		
+		out.println("<td>");
+		out.println("user");
+		out.println("</td>");
+		out.println("<td>");
+		out.println("<input name=\"user\" />");
+		out.println("</td>");
+		
+		out.println("<td>");
+		out.println("password");
+		out.println("</td>");
+		out.println("<td>");
+		out.println("<input name=\"password\" type=\"password\"/>");
+		out.println("</td>");
+		
+		out.println("<td>");
+		out.println("host");
+		out.println("</td>");
+		out.println("<td>");
+		out.println("<input name=\"host\" />");
+		out.println("</td>");
+		
+		out.println("<td>");
+		out.println("port");
+		out.println("</td>");
+		out.println("<td>");
+		out.println("<input name=\"port\" />");
+		out.println("</td>");
+		
+		out.println("</tr>");
+		out.println("</table>");
+
+		out.println("<input type=\"submit\" value=\"submit\"/>");
+		out.println("</form></body></html>");
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -40,10 +82,12 @@ public class MyServlet extends HttpServlet {
 		
 		out.println("processing...........<br/>");
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		String url = req.getParameter("url");
-		String u=req.getParameter("u");
-		String p=req.getParameter("p");
-		//"jdbc:mysql://localhost/test?user=minty&password=greatsqldb";
+		String host = req.getParameter("host");
+		String port = req.getParameter("port");
+		String url="jdbc:mysql://"+host+":"+port;
+		String u=req.getParameter("user");
+		String p=req.getParameter("password");
+		
 		Connection conn = DriverManager.getConnection(url, u, p );
 		out.println("got connection...........<br/>");
 		DatabaseMetaData md = conn.getMetaData();
@@ -59,22 +103,26 @@ public class MyServlet extends HttpServlet {
 		ResultSetMetaData rsmd = rs.getMetaData();
 		int columnCount = rsmd.getColumnCount();
 		out.println("<table>");
-		out.println("<th colspan=\""+columnCount+"\">"+label+"</th>");
 		
+		out.println("<th><td colspan=\""+columnCount+"\">"+label+"</td></th>");
+		out.println("<th>");
 		for (int i = 1; i <= columnCount; i++) 
 		{
-			out.println("<th>");
+			out.println("<td>");
 			out.println(rsmd.getColumnName(i));
-			out.println("</th>");
+			out.println("</td>");
 		}
+		out.println("</th>");
 		while(rs.next())
 		{
+			out.println("<tr>");
 			for (int i = 1; i <= columnCount; i++) 
 			{
-				out.println("<tr>");
+				out.println("<td>");
 				out.println(rs.getString(i));
-				out.println("</tr>");
+				out.println("</td>");
 			}
+			out.println("</tr>");
 		}
 		out.println("</table>");
 	}
