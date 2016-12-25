4a4e36e4df634d3bd6b087d1eeed4fa34925cf83
raghuraman1 <raghuraman1@yahoo.com>
2016-12-26 02:03:05 +0530

    commit

--- src/main/java/myjdbceg/Application.java
+++ src/main/java/myjdbceg/Application.java
@@ -3,10 +3,13 @@ package myjdbceg;
 import javax.servlet.http.HttpServlet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 	@Bean
 	public HttpServlet foo() {
