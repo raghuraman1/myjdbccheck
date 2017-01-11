3671ec240bd29284743894cc7fc71e8de5a30a50
raghuraman1 <raghuraman1@yahoo.com>
2017-01-11 22:46:28 +0530

    commit

--- src/main/java/myjdbceg/Application.java
+++ src/main/java/myjdbceg/Application.java
@@ -1,11 +1,14 @@
 package myjdbceg;
 
+import javax.servlet.MultipartConfigElement;
 import javax.servlet.http.HttpServlet;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
@@ -14,15 +17,31 @@ import org.springframework.context.annotation.Bean;
 public class Application {
 	
 	@Bean
-	   public ServletRegistrationBean axisServletRegistrationBean() {
+	   public ServletRegistrationBean reg1() {
 	      ServletRegistrationBean registration = new ServletRegistrationBean(new MyServlet(), "/services");
 	     // registration.addUrlMappings("*.jws");
 	      return registration;
 	   }
+	
+	
+	 @Bean 
+	    public MultipartConfigElement multipartConfigElement() { 
+	        MultipartConfigFactory factory = new MultipartConfigFactory(); 
+	        factory.setMaxRequestSize("10MB");
+
+	        factory.setMaxFileSize("10MB");
+
+	        return factory.createMultipartConfig(); 
+	    } 
+	
 	@Bean
-	public HttpServlet foo() {
-	    return new MyServlet();
-	}
+	   public ServletRegistrationBean reg2() {
+	      ServletRegistrationBean registration = new ServletRegistrationBean(new MyServlet1(), "/services1");
+	      registration.setMultipartConfig(multipartConfigElement());
+	      // registration.addUrlMappings("*.jws");
+	      return registration;
+	   }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
---
+++ src/main/java/myjdbceg/MyServlet1.java
@@ -0,0 +1,184 @@
+package myjdbceg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+@WebServlet("/Check1")
+@MultipartConfig
+public class MyServlet1 extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body><form method=\"POST\"  enctype=\"multipart/form-data\">");
+		out.println("<table>");
+		out.println("<tr>");
+		
+		out.println("<td>");
+		out.println("user");
+		out.println("</td>");
+		out.println("<td>");
+		out.println("<input name=\"user\" />");
+		out.println("</td>");
+		out.println("</tr>");
+		
+		out.println("<tr>");
+		out.println("<td>");
+		out.println("password");
+		out.println("</td>");
+		out.println("<td>");
+		out.println("<input name=\"password\" type=\"password\"/>");
+		out.println("</td>");
+		out.println("</tr>");
+		
+		out.println("<tr>");
+		out.println("<td>");
+		out.println("host");
+		out.println("</td>");
+		out.println("<td>");
+		out.println("<input name=\"host\" />");
+		out.println("</td>");
+		out.println("</tr>");
+		
+		out.println("<tr>");
+		out.println("<td>");
+		out.println("port");
+		out.println("</td>");
+		out.println("<td>");
+		out.println("<input name=\"port\" />");
+		out.println("</td>");
+		
+		out.println("</tr>");
+		
+		out.println("<tr>");
+		out.println("<td>");
+		out.println("file");
+		out.println("</td>");
+		out.println("<td>");
+		out.println("<input name=\"file\" type=\"file\"/>");
+		out.println("</td>");
+		out.println("</tr>");
+		out.println("</table>");
+
+		out.println("<input type=\"submit\" value=\"submit\"/>");
+		out.println("</form></body></html>");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body>");
+		out.println("<head>");
+		out.println("<style>");
+		out.println("table {");
+		out.println("    border-collapse: collapse;");
+		out.println("}");
+		out.println("");
+		out.println("table, td, th {");
+		out.println("    border: 1px solid black;");
+		out.println("}");
+		out.println("</style>");
+		out.println("</head>");
+		try {
+			process(out, req);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace(out);
+			
+		}
+		finally
+		{
+			out.println("</body></html>");
+		}
+	}
+	private String getFileName(final Part part) {
+	    final String partHeader = part.getHeader("content-disposition");
+	    
+	    for (String content : part.getHeader("content-disposition").split(";")) {
+	        if (content.trim().startsWith("filename")) {
+	            return content.substring(
+	                    content.indexOf('=') + 1).trim().replace("\"", "");
+	        }
+	    }
+	    return null;
+	}
+	private void process(PrintWriter out, HttpServletRequest req) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, IOException, ServletException {
+		
+		out.println("processing...........<br/>");
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		String host = req.getParameter("host");
+		String port = req.getParameter("port");
+		String url="jdbc:mysql://"+host+":"+port;
+		String u=req.getParameter("user");
+		String p=req.getParameter("password");
+		final Part filePart = req.getPart("file");
+	    final String fileName = getFileName(filePart);
+	    
+	    InputStream inputStream = filePart.getInputStream();
+	    InputStreamReader isr= new InputStreamReader(inputStream);
+	    BufferedReader br= new BufferedReader(isr);
+	    for (String line=br.readLine(); line!=null; line=br.readLine()) 
+	    {
+	    	out.println(line+"<br/>");
+		}
+		br.close();
+		Connection conn = DriverManager.getConnection(url, u, p );
+		out.println("got connection...........<br/>");
+		DatabaseMetaData md = conn.getMetaData();
+		
+		
+		showRs(out, md.getCatalogs(),  "catalogs");
+		showRs(out, md.getSchemas(),  "schemas");
+		showRs(out, md.getTables(null, null, null, null),  "tables");
+	
+		
+	}
+
+	private void showRs(PrintWriter out, ResultSet rs,  String label) throws SQLException {
+		ResultSetMetaData rsmd = rs.getMetaData();
+		int columnCount = rsmd.getColumnCount();
+		out.println(label+"<hr/>");
+		out.println("<table>");
+		
+		out.println("<tr><th colspan=\""+columnCount+"\">"+label+"</th></tr>");
+		out.println("<tr>");
+		for (int i = 1; i <= columnCount; i++) 
+		{
+			out.println("<th>");
+			out.println(rsmd.getColumnName(i));
+			out.println("</th>");
+		}
+		out.println("</tr>");
+		while(rs.next())
+		{
+			out.println("<tr>");
+			for (int i = 1; i <= columnCount; i++) 
+			{
+				out.println("<td>");
+				out.println(rs.getString(i));
+				out.println("</td>");
+			}
+			out.println("</tr>");
+		}
+		out.println("</table>");
+	}
+
+}
